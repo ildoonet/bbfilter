4e036b18bc089d489184b659bea7852f86c0daff
Curtis <curtis@kakao.com>
2014-01-15 16:04:26 +0900

    application project created

---
+++ BBFilter/src/com/ildoo/bbfilter/FilterApp.java
@@ -0,0 +1,32 @@
+package com.ildoo.bbfilter;
+
+import net.rim.device.api.ui.UiApplication;
+
+/**
+ * This class extends the UiApplication class, providing a
+ * graphical user interface.
+ */
+public class FilterApp extends UiApplication
+{
+    /**
+     * Entry point for application
+     * @param args Command line arguments (not used)
+     */ 
+    public static void main(String[] args)
+    {
+        // Create a new instance of the application and make the currently
+        // running thread the application's event dispatch thread.
+        FilterApp theApp = new FilterApp();       
+        theApp.enterEventDispatcher();
+    }
+    
+
+    /**
+     * Creates a new FilterApp object
+     */
+    public FilterApp()
+    {        
+        // Push a screen onto the UI stack for rendering.
+        pushScreen(new FilterScreen());
+    }    
+}
---
+++ BBFilter/src/com/ildoo/bbfilter/FilterScreen.java
@@ -0,0 +1,19 @@
+package com.ildoo.bbfilter;
+
+import net.rim.device.api.ui.container.MainScreen;
+
+/**
+ * A class extending the MainScreen class, which provides default standard
+ * behavior for BlackBerry GUI applications.
+ */
+public final class FilterScreen extends MainScreen
+{
+    /**
+     * Creates a new FilterScreen object
+     */
+    public FilterScreen()
+    {        
+        // Set the displayed title of the screen       
+        setTitle("MyTitle");
+    }
+}
